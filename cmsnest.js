7866c58d176d29542c1612978a53b40a5ac0d100
rkstpwlwo <rkstpwlwo@gmail.com>
2023-10-03 14:55:35 +0900

    thinkpad again again

--- cmsnest.js
+++ cmsnest.js
@@ -5,7 +5,7 @@ console.log("create my-feature branch");
 console.log("feature branch work 1");
 console.log("feature branch work 2");
 console.log("feature branch work 3");
-console.log("conflict mac2 THINKPAD again");
+console.log("conflict mac2 THINKPAD again again");
 
 (() => {
   var dt = Object.defineProperty;
